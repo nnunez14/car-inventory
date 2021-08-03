1c2e5b12d66561375eca7e464d9b60b923ea61f6
nnunez14 <85801701+nnunez14@users.noreply.github.com>
2021-08-02 23:31:58 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# Users/nathannunez/Desktop/operators-68/week-5/in-class
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Gives access to the project in ANY OS we find ourselves in
+# Allows outside files/folders to be added to the project from 
+# the base directory
+
